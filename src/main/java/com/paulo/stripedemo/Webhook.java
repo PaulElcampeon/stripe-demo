e40f83a2fc04c9a21095a1dea2b09722e0f5814c
PaulO <pauloladele@live.co.uk>
2024-07-27 11:54:24 +0100

    Log the session object

--- src/main/java/com/paulo/stripedemo/Webhook.java
+++ src/main/java/com/paulo/stripedemo/Webhook.java
@@ -3,6 +3,7 @@ package com.paulo.stripedemo;
 import com.stripe.model.Event;
 import com.stripe.model.EventDataObjectDeserializer;
 import com.stripe.model.StripeObject;
+import com.stripe.model.checkout.Session;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,11 +22,24 @@ public class Webhook {
     public ResponseEntity charge(@RequestBody Event event) {
         log.info("Webhook event: {}", event);
 
-        EventDataObjectDeserializer deserializer = event.getDataObjectDeserializer();
+        if ("checkout.session.completed".equals(event.getType()) || "checkout.session.async_payment_succeeded".equals(event.getType())) {
+            Optional<StripeObject> stripeObject = event.getDataObjectDeserializer().getObject();
 
-        Optional<StripeObject> stripeObject = deserializer.getObject();
+            if (stripeObject.isPresent()) {
+                Session session = (Session) stripeObject.get();
 
-        stripeObject.ifPresent(stripeObject1 -> log.info("Stripe Object: {}", stripeObject1));
+                log.info("Session: {}", session);
+
+
+            }
+//            fulfillCheckout(sessionEvent.getId());
+        }
+
+//        EventDataObjectDeserializer deserializer = event.getDataObjectDeserializer();
+//
+//        Optional<StripeObject> stripeObject = deserializer.getObject();
+
+//        stripeObject.ifPresent(stripeObject1 -> log.info("Stripe Object: {}", stripeObject1));
 
         return ResponseEntity.ok().build();
     }
