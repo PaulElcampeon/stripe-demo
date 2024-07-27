800bf22e5363e2c220315a96906af48c94970879
PaulO <pauloladele@live.co.uk>
2024-07-27 10:42:13 +0100

    Logging stripe object

--- src/main/java/com/paulo/stripedemo/Webhook.java
+++ src/main/java/com/paulo/stripedemo/Webhook.java
@@ -1,12 +1,16 @@
 package com.paulo.stripedemo;
 
 import com.stripe.model.Event;
+import com.stripe.model.EventDataObjectDeserializer;
+import com.stripe.model.StripeObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/webhook")
@@ -15,7 +19,14 @@ public class Webhook {
 
     @PostMapping("/test")
     public ResponseEntity charge(@RequestBody Event event) {
-        log.info("Webhook event: {}\nObject: {}", event, event.getObject());
+        log.info("Webhook event: {}", event);
+
+        EventDataObjectDeserializer deserializer = event.getDataObjectDeserializer();
+
+        Optional<StripeObject> stripeObject = deserializer.getObject();
+
+        stripeObject.ifPresent(stripeObject1 -> log.info("Stripe Object: {}", stripeObject1));
+
         return ResponseEntity.ok().build();
     }
 }
